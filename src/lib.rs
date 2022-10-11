e85c0b1e7a7551dee1e272a2406639b9e1b29f87
Ahmed Masud <ahmed.masud@saf.ai>
2022-10-11 02:14:29 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+//! Builder binding for Stores and Store managers
+//! This ensures that every manager is buildable.
+//! We use this in conjunction with builder pattern to create
+//! a new object.
+
+// #[cfg(feature = "serde")]
+// use serde::{Deserialize, Serialize};
+
+/// Build function trait for storage drivers.
+pub trait Buildable {
+    /// A builder type for this driver.
+    type Builder: Default;
+
+    /// Build Errors.
+    type Error: std::error::Error;
+
+    /// Provide a builder for this concrete type.
+    fn builder() -> Self::Builder {
+        Self::Builder::default()
+    }
+
+    /// Build from the builder.
+    fn build(builder: Self::Builder) -> Result<Self, Self::Error>
+    where
+        Self: Sized;
+}
